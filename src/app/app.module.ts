9c40b353c159411c402e96c4bc1b156f83ea1c5e
adovax <mailstodeepakdev@gmail.com>
2020-11-08 22:54:31 +0530

    New home design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
-
+import { HomeComponent } from './Dk/home/home.component';
+import { HeaderComponent } from './Dk/header/header.component';
+import { FooterComponent } from './Dk/footer/footer.component';
+import { ComplaintComponent } from './Dk/complaint/complaint.component';
+import { ContactComponent } from './Dk/contact/contact.component';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ComplaintComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
